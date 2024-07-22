cbcc33909872a774554a24263bb0144e9592aef7
Vincent Lyondo <88526388+vincelyon@users.noreply.github.com>
2024-07-23 00:38:01 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Me from '../../assets/image.webp'
+import Me from '../../assets/Vincent.jpg'
 import Socials from './Headersocials'
 
 const Header = () => {
